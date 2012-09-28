c39614bd1355bc89809a809ab9415246e74807dd
Pas <pascal@offenes-lernen.ch>
2012-09-28 18:59:31 +0200

    Fixed ItemTest
    - Fixed paths
    - New method get_item_house for faster adapting
    
    modified:   test/item_test.rb

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,36 +1,38 @@
+require "rubygems"
+require "require_relative"
 require "test/unit"
-require "../app/University/item"
-require "../app/University/user"
+require "../app/models/item"
+require "../app/models/user"
 
 class ItemTest < Test::Unit::TestCase
-  def test_should_have_a_name
+  def get_item_house
     user = create_user()
-    item = University::Item.create(user, 'house', 100)
+    Item.create(user, 'house', 100)
+  end
+
+  def test_should_have_a_name
+    item =  get_item_house
     assert( item.name == 'house', "Item should be called \'house\' but was #{item.name} instead")
   end
 
   def test_should_have_a_price
-    user = create_user()
-    item = University::Item.create(user, 'house', 100)
+    item =  get_item_house
     assert( item.price == 100, "Item should cost \'100\' but was #{item.price} instead")
   end
 
   def test_should_be_inactive_start
-    user = create_user()
-    item = University::Item.create(user, 'house', 100)
+    item =  get_item_house
     assert(! item.is_active?, 'Item should be inactive after creation.')
   end
 
   def test_should_be_active
-    user = create_user()
-    item = University::Item.create(user, 'house', 100)
+    item =  get_item_house
     item.activate()
     assert(item.is_active?, 'Item should be activated.')
   end
 
   def test_should_be_inactive
-    user = create_user()
-    item = University::Item.create(user, 'house', 100)
+    item =  get_item_house
     item.activate()
     assert(item.is_active?, 'Item should be activated.')
     item.deactivate()
@@ -38,12 +40,13 @@ class ItemTest < Test::Unit::TestCase
   end
 
   def test_item_should_have_owner
-    user = create_user()
-    item = University::Item.create(user, 'house', 100)
+    user = User.named('john', 'nhoj')
+    item = Item.create(user, 'house', 100)
     assert(item.owner == user, "Jane should be owner.")
   end
 
   def create_user()
-    University::User.named('Jane')
+    User.named('Jane', 'jane')
   end
+
 end
